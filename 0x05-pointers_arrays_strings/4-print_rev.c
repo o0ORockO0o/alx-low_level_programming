ab5438cc278b6e2df92ede3923ba108d6a3c0b1a
o0ORockO0o <77933790+o0ORockO0o@users.noreply.github.com>
2022-06-15 00:02:36 +0200

    Task_submission

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,22 @@
-a
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
