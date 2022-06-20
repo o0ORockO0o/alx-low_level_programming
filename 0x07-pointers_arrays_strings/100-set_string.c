1d572dfcf308e129666b3a7981f7c424c45f39c0
o0ORockO0o <77933790+o0ORockO0o@users.noreply.github.com>
2022-06-21 01:36:41 +0200

    Task Submission

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,20 @@
 #include "main.h"
 #include <stddef.h>
 /**
-* set_string - a function that sets the value of a pointer to a char.
-*@s: pointer value
-*@to: value pointed to
-*
-*/
-void set_string(char **s, char *to)
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character
+ *
+ * Return: a pointer to a character aka the character found
+ */
+char *_strchr(char *s, char c)
 {
-	*s = to;
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
 }
