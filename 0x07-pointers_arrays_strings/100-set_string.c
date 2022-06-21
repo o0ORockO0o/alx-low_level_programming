7350f8875d3ff5089ead36663d6b829d0f9a299b
o0ORockO0o <77933790+o0ORockO0o@users.noreply.github.com>
2022-06-21 03:07:36 +0200

    Task Submission

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * set_string - function that sets the value of a pointer to a char
- * @s: address of pointer
- * @to: address of value
+ * set_string - redirect.
+ * @s: double pointer to address
+ * @to: the original pointer
+ *
+ * Return: chessboard.
  */
 void set_string(char **s, char *to)
 {
