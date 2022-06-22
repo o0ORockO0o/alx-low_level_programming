bc6d23d83dc45d445b152bb973d7c8b58789196a
o0ORockO0o <77933790+o0ORockO0o@users.noreply.github.com>
2022-06-23 00:33:35 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
-#incilude "main.h"
-
+#include "main.h"
 /**
- *  _puts_recursion -> this is a function to print string recursivly
- *  @s: string parameter
+ * _puts_recursion - writes the character n to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
+	_putchar('\n');
+	return;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
-}	
+_putchar(*s);
+
+_puts_recursion(s + 1);
+}
